61fcf1cba0b9b33b5bafc11c4c8ac2ca8bc82921
Fabian Cook <hello@fabiancook.dev>
2023-06-23 22:49:03 +1200

    Order items API, products/offers, #4

--- src/listen/order-item/add-order-item.ts
+++ src/listen/order-item/add-order-item.ts
@@ -1,18 +1,17 @@
 import { FastifyInstance } from "fastify";
-import {addOrderItem, getOrder, OrderItemData, orderItemSchema} from "../../data";
+import {addOrderItem, getOffer, getOrder, getProduct, OrderItemData, orderItemSchema} from "../../data";
 import { authenticate } from "../authentication";
 import {getMaybePartner, getMaybeUser} from "../../authentication";
 
 export async function addOrderItemRoutes(fastify: FastifyInstance) {
 
-  type Params = {
-    orderId: string
+  type Querystring = {
+    redirect?: string;
   }
 
-  type Schema = {
-    Params: Params
-    Body: OrderItemData;
-  };
+  type OrderParams = {
+    orderId: string
+  }
 
   const response = {
     201: {
@@ -21,20 +20,37 @@ export async function addOrderItemRoutes(fastify: FastifyInstance) {
     },
   };
 
-  const schema = {
-    description: "Add a new order item",
-    tags: ["order item"],
-    summary: "",
-    body: orderItemSchema.orderItemData,
-    response,
-    security: [
-      {
-        apiKey: [] as string[],
-      },
-    ],
-  };
-
   try {
+
+    type Schema = {
+      Querystring: Querystring
+      Params: OrderParams
+      Body: OrderItemData;
+    };
+
+    const params = {
+      type: "object",
+      properties: {
+        orderId: {
+          type: "string"
+        }
+      },
+      required: ["orderId"]
+    };
+
+    const schema = {
+      description: "Add a new order item",
+      tags: ["order item"],
+      summary: "",
+      body: orderItemSchema.orderItemData,
+      response,
+      params,
+      security: [
+        {
+          apiKey: [] as string[],
+        },
+      ],
+    };
     fastify.post<Schema>("/", {
       schema,
       preHandler: authenticate(fastify),
@@ -52,6 +68,167 @@ export async function addOrderItemRoutes(fastify: FastifyInstance) {
           ...request.body,
           orderId: request.params.orderId
         });
+        const { redirect } = request.query;
+        if (redirect) {
+          const url = redirect.replace(":orderItemId", orderItem.orderItemId);
+          response.header("Location", url);
+          response.status(302);
+          response.send();
+          return;
+        }
+        response.status(201);
+        response.send(orderItem);
+      },
+    });
+  } catch {}
+
+  try {
+    type Params = OrderParams & {
+      offerId: string;
+    }
+
+    type Schema = {
+      Querystring: Querystring
+      Params: Params
+      Body: OrderItemData;
+    };
+
+    const params = {
+      type: "object",
+      properties: {
+        orderId: {
+          type: "string"
+        },
+        offerId: {
+          type: "string"
+        }
+      },
+      required: ["orderId", "offerId"]
+    };
+
+    const schema = {
+      description: "Add a new order item",
+      tags: ["order item"],
+      summary: "",
+      body: orderItemSchema.orderItemData,
+      response,
+      params,
+      security: [
+        {
+          apiKey: [] as string[],
+        },
+      ],
+    };
+    fastify.post<Schema>("/offers/:offerId", {
+      schema,
+      preHandler: authenticate(fastify),
+      async handler(request, response) {
+        const order = await getOrder(request.params.orderId, {
+          userId: getMaybeUser()?.userId,
+          organisationId: getMaybePartner()?.organisationId
+        });
+        if (!order) {
+          response.status(404);
+          response.send();
+          return;
+        }
+        const offer = await getOffer(request.params.offerId)
+        if (!offer) {
+          response.status(404);
+          response.send();
+          return;
+        }
+        const orderItem = await addOrderItem({
+          ...request.body,
+          productId: undefined,
+          offerId: offer.offerId,
+          orderId: request.params.orderId
+        });
+        const { redirect } = request.query;
+        if (redirect) {
+          const url = redirect.replace(":orderItemId", orderItem.orderItemId);
+          response.header("Location", url);
+          response.status(302);
+          response.send();
+          return;
+        }
+        response.status(201);
+        response.send(orderItem);
+      },
+    });
+  } catch {}
+
+
+  try {
+    type Params = OrderParams & {
+      productId: string;
+    }
+
+    type Schema = {
+      Querystring: Querystring;
+      Params: Params;
+      Body: OrderItemData;
+    };
+
+    const params = {
+      type: "object",
+      properties: {
+        orderId: {
+          type: "string"
+        },
+        productId: {
+          type: "string"
+        }
+      },
+      required: ["orderId", "productId"]
+    };
+
+    const schema = {
+      description: "Add a new order item",
+      tags: ["order item"],
+      summary: "",
+      body: orderItemSchema.orderItemData,
+      response,
+      params,
+      security: [
+        {
+          apiKey: [] as string[],
+        },
+      ],
+    };
+    fastify.post<Schema>("/products/:productId", {
+      schema,
+      preHandler: authenticate(fastify),
+      async handler(request, response) {
+        const order = await getOrder(request.params.orderId, {
+          userId: getMaybeUser()?.userId,
+          organisationId: getMaybePartner()?.organisationId
+        });
+        if (!order) {
+          response.status(404);
+          response.send();
+          return;
+        }
+        const product = await getProduct(request.params.productId)
+        if (!product) {
+          response.status(404);
+          response.send();
+          return;
+        }
+        const orderItem = await addOrderItem({
+          ...request.body,
+          offerId: undefined,
+          productId: product.productId,
+          orderId: request.params.orderId
+        });
+        const { redirect } = request.query;
+        if (redirect) {
+          const url = redirect.replace(":orderItemId", orderItem.orderItemId);
+          response.header("Location", url);
+          response.status(302);
+          response.send();
+          return;
+        }
         response.status(201);
         response.send(orderItem);
       },
