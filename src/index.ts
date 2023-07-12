51258e85985a2cba80798b085c4d1d1f272f7ca8
Fabian Cook <hello@fabiancook.dev>
2023-07-12 21:32:09 +1200

    Add additional exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import "./references";
+
 export * from "./data";
 export * from "./config";
 export * from "./listen";
 export * from "./view";
 export * from "./package";
 export * from "./react/server";
+export * as authentication from "./authentication";
+export * from "./remote";
+export * from "./is";
--- src/listen/index.ts
+++ src/listen/index.ts
@@ -1,2 +1,4 @@
 export * from "./start";
 export * from "./routes";
+export * from "./authentication";
+export * from "./config";
