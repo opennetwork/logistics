300bb0efb999048d2005286a7b74b968819f240f
Fabian Cook <hello@fabiancook.dev>
2023-09-11 16:29:21 +1200

    Update bun require.cache example

--- example.mjs
+++
@@ -1,9 +0,0 @@
-import * as mod from "./esnext/index.js";
-
-if (!require.cache) {
-    throw new Error("No require.cache");
-} else {
-    console.log("require.cache exists")
-}
-
-console.log(!!mod);
