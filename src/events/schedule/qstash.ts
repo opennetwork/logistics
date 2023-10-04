345e82df1ad416427cf072c6ececaa355504369f
Fabian Cook <hello@fabiancook.dev>
2023-10-05 00:41:05 +1300

    QStash v2

--- src/events/schedule/qstash.ts
+++ src/events/schedule/qstash.ts
@@ -39,44 +39,11 @@ function isQStashV1() {
 function getQStashURL() {
     const { QSTASH_URL } = process.env;
     const baseUrl = new URL(QSTASH_URL || "https://qstash.upstash.io/v2");
-    if (!baseUrl.pathname.endsWith("/")) {
+    if (baseUrl.pathname.endsWith("/")) {
         baseUrl.pathname = baseUrl.pathname.substring(0, baseUrl.pathname.length - 1);
     }
     ok(baseUrl.pathname.endsWith("/v2"), "Expected v2 qstash API");
     return baseUrl;
-}
-
-function getQStashDurableEventSchedule(event: DurableEventData) {
-    const { cron, after, delay, before, timezone } = event.schedule ?? {};
-    if (cron) {
-        return {
-            cron
-        }
-    }
-    if (after) {
-        return {
-            after,
-            timezone
-        }
-    }
-    if (delay) {
-        return {
-            delay
-        }
-    }
-    if (before) {
-        throw new Error("Not implemented in QStash Schedule, please raise a ticket in this implementing module");
-    }
-
-    const {QSTASH_DELAY} = process.env;
-    if (QSTASH_DELAY) {
-        return {
-            delay: QSTASH_DELAY
-        }
-    }
-
-    return undefined;
-
 }
 
 export async function dispatchQStash(event: DurableEventData) {
@@ -91,7 +58,8 @@ export async function dispatchQStash(event: DurableEventData) {
         QSTASH_EVENT_URL,
         QSTASH_TOKEN,
         QSTASH_RETRIES,
-        QSTASH_CRON_RETRIES
+        QSTASH_CRON_RETRIES,
+        QSTASH_DELAY
     } = process.env;
 
     const store = getMetaStore(event);
@@ -113,33 +81,33 @@ export async function dispatchQStash(event: DurableEventData) {
 
     let isSingleMessage = true;
 
-    const schedule = getQStashDurableEventSchedule(event);
+    const { schedule } = event;
 
-    if (schedule?.after) {
+    if (schedule?.cron) {
+        // https://upstash.com/docs/qstash/features/schedules
+        // It can take up to 60 seconds for the schedule to be loaded on an active node and triggered for the first time.
+        headers.set("Upstash-Cron", schedule.cron);
+        if (QSTASH_CRON_RETRIES) {
+            headers.set("Upstash-Retries", QSTASH_CRON_RETRIES);
+        }
+        isSingleMessage = false;
+    } else if (schedule?.after) {
         // https://upstash.com/docs/qstash/features/delay#absolute-delay
         // The format is a unix timestamp in seconds, based on the UTC timezone.
         const seconds = new Date(schedule.after).getTime() / 1000;
         const rounded = Math.round(seconds);
         headers.set("Upstash-Not-Before", rounded.toString());
-    } else {
-        if (schedule?.cron) {
-            // https://upstash.com/docs/qstash/features/schedules
-            // It can take up to 60 seconds for the schedule to be loaded on an active node and triggered for the first time.
-            headers.set("Upstash-Cron", schedule.cron);
-            if (QSTASH_CRON_RETRIES) {
-                headers.set("Upstash-Retries", QSTASH_CRON_RETRIES);
-            }
-            isSingleMessage = false;
-        } else if (!headers.has("Upstash-Not-Before") && schedule?.delay) {
-            // https://upstash.com/docs/qstash/features/delay#relative-delay
-            // Upstash-Delay will get overridden by Upstash-Not-Before header when both are used together.
-            // Allows for a default delay
-            if (typeof schedule.delay === "string") {
-                headers.set("Upstash-Delay", schedule.delay);
-            } else {
-                headers.set("Upstash-Delay", `${schedule.delay}ms`);
-            }
-            isSingleMessage = false;
+    }
+
+    if (!headers.has("Upstash-Not-Before") && (schedule?.delay || QSTASH_DELAY)) {
+        // https://upstash.com/docs/qstash/features/delay#relative-delay
+        // Upstash-Delay will get overridden by Upstash-Not-Before header when both are used together.
+        // Allows for a default delay
+        const delay = schedule?.delay || QSTASH_DELAY;
+        if (typeof delay === "string") {
+            headers.set("Upstash-Delay", delay);
+        } else {
+            headers.set("Upstash-Delay", `${delay}ms`);
         }
     }
 
@@ -150,7 +118,8 @@ export async function dispatchQStash(event: DurableEventData) {
     if (isSingleMessage) {
         url.pathname = `${baseUrl.pathname}/publish/${targetUrl.toString()}`
     } else {
-        url.pathname = `${baseUrl.pathname}/schedules/${targetUrl.toString()}`
+        url.pathname = `${baseUrl.pathname}/schedules`;
+        headers.set("Upstash-Callback", targetUrl.toString());
     }
 
     const background: BackgroundQuery = {
@@ -169,7 +138,6 @@ export async function dispatchQStash(event: DurableEventData) {
     ok(response.ok, "Could not dispatch QStash message");
     const { messageId, scheduleId }: Partial<ScheduleMeta> = await response.json();
     const data = {
-        messageId,
         scheduleId,
         createdAt: new Date().toISOString()
     };
