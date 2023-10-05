afe92fafbbb788cca085e327e606c22069096745
Fabian Cook <hello@fabiancook.dev>
2023-10-05 17:10:30 +1300

    Update default AUTHSIGNAL_CHALLENGE_API_URL

--- src/authentication/authsignal.ts
+++ src/authentication/authsignal.ts
@@ -9,7 +9,7 @@ export const {
 } = process.env;
 
 export const AUTHSIGNAL_BASE_URL = process.env.AUTHSIGNAL_API_URL || "https://au.signal.authsignal.com/v1";
-export const AUTHSIGNAL_CHALLENGE_API_URL = process.env.AUTHSIGNAL_CHALLENGE_API_URL || "https://au-challenge.authsignal.com/v1";
+export const AUTHSIGNAL_CHALLENGE_API_URL = process.env.AUTHSIGNAL_CHALLENGE_API_URL || "https://au.api.authsignal.com/v1";
 
 export const authsignal = new Authsignal({
   secret: process.env.AUTHSIGNAL_SECRET,
