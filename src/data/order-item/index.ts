27086cdcd7928a3522b6882861b249d2d6dc4ba0
Fabian Cook <hello@fabiancook.dev>
2023-11-18 23:54:32 +1300

    getOrderPrice

--- src/data/order-item/index.ts
+++ src/data/order-item/index.ts
@@ -5,4 +5,5 @@ export * from "./add-order-item";
 export * from "./set-order-items";
 export * as orderItemSchema from "./schema";
 export * from "./get-order-item";
+export * from "./get-order-item-info";
 export * from "./delete-order-item";
